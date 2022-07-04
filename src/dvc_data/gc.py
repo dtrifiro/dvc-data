ce11657e495af5693762e298577e75c905721771
Daniele Trifirò <daniele@iterative.ai>
2022-07-04 16:18:11 +0200

    gc: use ThreadPoolExecutor

--- src/dvc_data/gc.py
+++ src/dvc_data/gc.py
@@ -1,4 +1,11 @@
-def gc(odb, used, jobs=None, cache_odb=None, shallow=True):
+from dvc_objects.executors import ThreadPoolExecutor
+
+from dvc_data.transfer import _log_exceptions
+
+
+def gc(
+    odb, used, jobs=None, cache_odb=None, shallow=True, progress_callback=None
+):
     from dvc_objects.errors import ObjectDBPermissionError
 
     from ._progress import QueryingProgress
@@ -25,16 +32,27 @@ def gc(odb, used, jobs=None, cache_odb=None, shallow=True):
     removed = False
     # hashes must be sorted to ensure we always remove .dir files first
 
-    hashes = QueryingProgress(odb.all(jobs), name=odb.path)
-    for hash_ in sorted(hashes, key=_is_dir_hash, reverse=True):
+    hashes_to_remove = {True: [], False: []}
+    for hash_ in QueryingProgress(odb.all(jobs), name=odb.path):
         if hash_ in used_hashes:
             continue
+        hashes_to_remove[_is_dir_hash(hash_)].append(hash_)
+
+    def remover(hash_):
         path = odb.oid_to_path(hash_)
         if _is_dir_hash(hash_):
             # backward compatibility
             # pylint: disable=protected-access
             odb._remove_unpacked_dir(hash_)
         odb.fs.remove(path)
-        removed = True
+        return 1
 
+    removed = 0
+    with ThreadPoolExecutor(max_workers=jobs) as executor:
+        for dir_hash in (True, False):
+            removed += sum(
+                executor.imap_unordered(
+                    _log_exceptions(remover), hashes_to_remove[dir_hash]
+                )
+            )
     return removed
